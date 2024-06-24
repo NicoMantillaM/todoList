5cfcbe16d73dfa5e5c1d8bc92a1bb3aca2631031
NicoMantillaM <160680324+NicoMantillaM@users.noreply.github.com>
2024-06-23 21:51:26 -0500

    feat: :sparkles: Metodo Post-Crear tareas
    Se actualizó index.html con nuevas entradas e ID de botones, main.js para manejar la creación y carga de tareas, y api.js para incluir la función addTask

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { getAllTask } from "./module/api.js";
+import { getAllTask, addTask } from "./module/api.js";
 import { task } from "./components/list.js";
 
 
@@ -13,11 +13,27 @@ document.addEventListener('DOMContentLoaded', async() => {
 
     let info = await getAllTask();
 
-    //crear un nuevo array con los elementos que cumplan la condicion
-    let taskOnHold = info.filter(task=> task.status === 'On hold');   
-    let taskready = info.filter(task=> task.status === 'ready');
+    let loadTasks = async() => {
+        //crear un nuevo array con los elementos que cumplan la condicion
+        let taskOnHold = info.filter(task=> task.status === 'On hold');   
+        let taskready = info.filter(task=> task.status === 'ready');
+    
+        section__onhold.innerHTML = await task(taskOnHold, 'On hold');
+        section__ready.innerHTML = await task(taskready, 'ready');
+    };
 
-    section__onhold.innerHTML = await task(taskOnHold, 'On hold');
-    section__ready.innerHTML = await task(taskready, 'ready');
+    await loadTasks();
+
+    let createTask = document.querySelector('#createTask');
+    createTask.addEventListener("click", async()=>{
+        let taskName = document.querySelector('#taskName');
+        let newTask = {
+            task: taskName.value,
+            status: 'On hold'
+        };
+        await addTask(newTask);
+        taskName.value = '';
+        await loadTasks();
+    });
 });
 
--- js/module/api.js
+++ js/module/api.js
@@ -7,3 +7,18 @@ export const getAllTask = async() => {
     return data;
 }
 
+export const addTask = async(task) => {
+    const url = ("https://66760010a8d2b4d072f22091.mockapi.io/Todolist")
+    const opcion = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(task)
+
+    };
+
+    const res = await fetch(url, opcion)
+    const data = await res.json();
+    return data;
+}
